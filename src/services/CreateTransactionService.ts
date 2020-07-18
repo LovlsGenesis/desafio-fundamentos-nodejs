4ddc1b254898e5a37978e4c598704132c7fff6fb
Guillaume Zeldine <g.zeldine@hotmail.fr>
2020-07-18 20:45:40 -0300

    Challenge done

--- src/services/CreateTransactionService.ts
+++ src/services/CreateTransactionService.ts
@@ -1,6 +1,12 @@
 import TransactionsRepository from '../repositories/TransactionsRepository';
 import Transaction from '../models/Transaction';
 
+interface Request {
+  title: string;
+  type: 'outcome' | 'income';
+  value: number;
+}
+
 class CreateTransactionService {
   private transactionsRepository: TransactionsRepository;
 
@@ -8,8 +14,23 @@ class CreateTransactionService {
     this.transactionsRepository = transactionsRepository;
   }
 
-  public execute(): Transaction {
-    // TODO
+  public execute({ title, type, value }: Request): Transaction {
+    if (!['income', 'outcome'].includes(type)) {
+      throw Error(`Transaction type invalid: ${type}`);
+    }
+    const { total } = this.transactionsRepository.getBalance();
+
+    if (total - value < 0 && type === 'outcome') {
+      throw Error("Can't make transaction. Your balance is negative");
+    }
+
+    const transaction = this.transactionsRepository.create({
+      title,
+      type,
+      value,
+    });
+
+    return transaction;
   }
 }
 
